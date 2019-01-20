4cd891dbfc40b9b908efbeb8755ae39705d2c709
ines.kulenovic <ines.kulenovic>
2019-01-20 22:12:36 +0100

    Updates

---
+++ precache-manifest.58902e1734707c4b99f0dd45fd2ae2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oblique-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "731cbb006555e86c1a8f",
+    "url": "/oblique-web/static/js/runtime~main.731cbb00.js"
+  },
+  {
+    "revision": "33dfe85786039a873484",
+    "url": "/oblique-web/static/js/main.33dfe857.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/oblique-web/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "33dfe85786039a873484",
+    "url": "/oblique-web/static/css/main.d36b4b82.chunk.css"
+  },
+  {
+    "revision": "f7f16addf8ea582a6f89e761946e2bac",
+    "url": "/oblique-web/index.html"
+  }
+];
